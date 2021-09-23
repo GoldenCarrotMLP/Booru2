6beaf6ab368cec778565294a8485d5beed482117
GoldenCarrotMLP <carrotgoldmlp@gmail.com>
2021-09-23 17:00:53 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,7 @@
-import asyncio
-import aiohttp
-import json
+var headers = {'user-agent': 'my-app/0.0.1 goldencarrot'}
+var url = 'https://e621.net/posts.json?'
+var auth = 'goldencarrot cEgqVRkzauWzTtBmbz32aLLf'
 
-headers = {
-  'User-Agent': 'my-app/0.0.1 goldencarrot',
-  'Authorization': 'goldencarrot cEgqVRkzauWzTtBmbz32aLLf'
-}
-
-async def main():
-  async with aiohttp.ClientSession(headers=headers) as session:
-    url = 'https://e621.net/posts.json?limit=2&'
-
-    async with session.get(url) as resp:
-      resp = await resp.json(content_type=None)
-      print(json.dumps(resp.get('posts')[0], indent=2))
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data));
