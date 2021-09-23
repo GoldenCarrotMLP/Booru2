afa528bf47eb27038df695f98324dbe3fc251ba9
GoldenCarrotMLP <carrotgoldmlp@gmail.com>
2021-09-23 17:40:24 -0400

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
-var headers = {'user-agent': 'my-app/0.0.1 goldencarrot'}
-var url = 'https://e621.net/posts.json?'
-var auth = 'goldencarrot cEgqVRkzauWzTtBmbz32aLLf'
-tags = 'tags=fluffy+rating:s&limit=1'
 
-fetch(url+tags)
+var url = 'https://e621.net/posts.json?'
+var auth = 'goldencarrot:cEgqVRkzauWzTtBmbz32aLLf'
+var tags = 'tags=fluffy+rating:s&limit=1'
+var pic = ''
+
+fetch(url+tags,{headers: new Headers({'Authorization': 'Basic '+btoa(auth)})})
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .then(data => pic);
+  //document.getElementById("d").alt=pic;
+  console.log(pic)
+  
