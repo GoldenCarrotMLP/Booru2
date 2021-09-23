7e132bcf1ae984163d408dd6dc0c4a5da3a1fa4b
GoldenCarrotMLP <carrotgoldmlp@gmail.com>
2021-09-23 17:11:31 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 var headers = {'user-agent': 'my-app/0.0.1 goldencarrot'}
 var url = 'https://e621.net/posts.json?'
 var auth = 'goldencarrot cEgqVRkzauWzTtBmbz32aLLf'
+tags = 'tags=fluffy+rating:s&limit=1'
 
-fetch(url)
+fetch(url+tags)
   .then(response => response.json())
   .then(data => console.log(data));
