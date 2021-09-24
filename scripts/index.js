4b95bec5d2b3c45cee3509e77e4b4bdf0fd3d2ad
GoldenCarrotMLP <carrotgoldmlp@gmail.com>
2021-09-23 21:11:14 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,8 @@ var pic = ''
 
 fetch(url+tags,{headers: new Headers({'Authorization': 'Basic '+btoa(auth)})})
   .then(response => response.json())
-  .then(data => console.log(data))
-  .then(data => pic);
+ // .then(data => console.log(data))
+  .then(data => pic = data);
   //document.getElementById("d").alt=pic;
-  console.log(pic)
-  
+  console.log(pic.posts[0].file.url);
+  console.log(pic.posts[0].file.url);
