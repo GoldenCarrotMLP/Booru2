824cfe8b1f4afb010a5c893cdf626377b01d9ff6
GoldenCarrotMLP <carrotgoldmlp@gmail.com>
2021-09-23 22:23:25 -0400

    q

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,21 @@
 
 var url = 'https://e621.net/posts.json?'
 var auth = 'goldencarrot:cEgqVRkzauWzTtBmbz32aLLf'
-var tags = 'tags=fluffy+rating:s&limit=1'
+
 var pic = ''
 
-fetch(url+tags,{headers: new Headers({'Authorization': 'Basic '+btoa(auth)})})
+
+var a = pic;
+
+
+function myFunction() {
+  var tags = document.getElementById("tags").value
+   var getTags= "tags="+ tags.replace(' ', '+') + "&limit=1";
+  fetch(url+tags,{headers: new Headers({'Authorization': 'Basic '+btoa(auth)})})
   .then(response => response.json())
  // .then(data => console.log(data))
-  .then(data => pic = data);
-  //document.getElementById("d").alt=pic;
-  console.log(pic.posts[0].file.url);
-  console.log(pic.posts[0].file.url);
+  .then(data => pic = data.posts[0].file.url);
+  
+
+    document.getElementById("button").src=pic;
+}
